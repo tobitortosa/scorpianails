3954229f0a23ce09195204fc3e151145510cd93a
EC45326328 <ttortosa@ecosistemas.com.ar>
2025-02-10 20:04:30 -0300

    cambios

--- src/components/PaymentForm/PaymentForm.jsx
+++ src/components/PaymentForm/PaymentForm.jsx
@@ -6,6 +6,23 @@ function PaymentForm() {
   const [file, setFile] = useState(null);
   const fileInputRef = useRef(null);
 
+  const precio = "$4500";
+
+  const [email, setEmail] = useState("");
+  const [isEmailValid, setIsEmailValid] = useState(true);
+  const [phone, setPhone] = useState("");
+  const [isPhoneValid, setIsPhoneValid] = useState(true);
+
+  const validateEmail = (value) => {
+    const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    setIsEmailValid(regex.test(value));
+  };
+
+  const validatePhone = (value) => {
+    const regex = /^(\+\d{1,3}\s?)?\d{1,4}\s?\d{4,5}\s?\d{4}$/;
+    setIsPhoneValid(regex.test(value) && value.length >= 10);
+  };
+
   const handleChange = (event) => {
     const selectedFile = event.target.files[0];
     if (selectedFile) {
@@ -34,13 +51,32 @@ function PaymentForm() {
   const handleSubmit = (e) => {
     e.preventDefault();
     if (file) {
-      const formData = new FormData();
-      formData.append("comprobante", file);
+      const validTypes = [
+        "image/png",
+        "image/jpeg",
+        "image/jpg",
+        "application/pdf",
+      ];
 
-      console.log("Comprobante recibido:", file.name);
-      alert("Comprobante recibido. El PDF se enviará pronto.");
-    } else {
-      alert("Por favor, sube un comprobante de pago.");
+      if (!validTypes.includes(file.type)) {
+        alert("Solo puedes subir archivos en formato PNG, JPG o PDF.");
+        setFile(null);
+        return;
+      }
+
+      if (!isEmailValid) {
+        alert("Ingrese un email correcto.");
+        setIsEmailValid(false);
+        return;
+      }
+
+      if (!isPhoneValid) {
+        alert("Ingrese un numero de telefono correcto.");
+        setIsPhoneValid(false);
+        return;
+      }
+
+      console.log("Archivo subido y datos correctos");
     }
   };
 
@@ -48,79 +84,91 @@ function PaymentForm() {
     <section className={styles.section}>
       <h2 className={styles.title}>Realiza tu pago y obtén el PDF</h2>
       <div className={styles.formContainer}>
+        <h3 style={{ textAlign: "center", color: "#00ee08e0" }}>
+          Precio: {precio} ARS
+        </h3>
         <h3 style={{ textAlign: "center" }}>
           Querés mejorar tus habilidades en Manicuría?
         </h3>
-        <h4>
+        <h4 style={{ textAlign: "center" }}>
           En este pdf exclusivo vas a encontrar técnicas y consejos
           profesionales para llevar tus habilidades a otro nivel. Ideal para
-          quienes quieran recién empiezan. Aprende paso a paso como ofrecer un
-          servicio impecable y duradero para traerlos devuelta contigo. No
-          esperes más para convertirte en esa Manicura que tanto soñaste ser!
+          quienes recien empiezan. Aprende paso a paso como ofrecer un servicio
+          impecable y duradero para traerlos devuelta contigo.
+          <p>
+            No esperes más para convertirte en esa Manicura que tanto soñaste
+            ser!
+          </p>
         </h4>
         <div className={styles.contactoCliente}>
-          <h2>Primer paso:</h2>
+          <h2>1) Primer paso:</h2>
           <h3>
             Ingresa un mail y un numero de telefono donde vas a querer recibir
             la guia:
           </h3>
 
-          <label>Email:</label>
-          <input type="text" placeholder="@gmail.com" />
+          <div className={styles.mailContainer}>
+            <label className={styles.mailLabel}>Email:</label>
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => {
+                setEmail(e.target.value);
+                validateEmail(e.target.value);
+              }}
+              placeholder="usuario@gmail.com"
+              className={`${styles.mailInput} ${
+                isEmailValid ? styles.mailValid : styles.mailInvalid
+              }`}
+            />
+            {!isEmailValid && (
+              <p className={styles.mailErrorText}>
+                Ingresa un correo válido de Gmail.
+              </p>
+            )}
+          </div>
 
-          <label>Numero de teléfono:</label>
-          <input type="text" placeholder="+54 11 1234-5678" />
+          <div className={styles.phoneContainer}>
+            <label className={styles.phoneLabel}>Número de Teléfono:</label>
+            <input
+              type="tel"
+              value={phone}
+              onChange={(e) => {
+                setPhone(e.target.value);
+                validatePhone(e.target.value);
+              }}
+              placeholder="11 1234-5678"
+              className={`${styles.phoneInput} ${
+                isPhoneValid ? styles.phoneValid : styles.phoneInvalid
+              }`}
+            />
+            {!isPhoneValid && (
+              <p className={styles.phoneError}>
+                Formato inválido. <br /> Ej: +54 11 40851865 o 1140851865
+              </p>
+            )}
+            <p style={{ textAlign: "center" }}>
+              Si tu número no es de Argentina, recuerda incluir el código de
+              país.
+            </p>
+          </div>
         </div>
 
         <div className={styles.contactoCliente}>
-          <h2>Segundo paso:</h2>
+          <h2>2) Segundo paso:</h2>
           <h3 className={styles.text}>
-            Realiza una transferencia a la siguiente cuenta bancaria y subí el
-            comprobante:
+            Realiza una transferencia por {precio} a la siguiente cuenta
+            bancaria y subí el comprobante:
           </h3>
           <p className={styles.cbu}>Alias: rocio.moire</p>
           <p className={styles.cbu} style={{ marginBottom: "15px" }}>
             CBU: 1234 5678 9000 1111 2222
           </p>
           <p htmlFor="comprobante" className={styles.label}>
-            ADJUNTA EL COMPROBANTE ACA:
+            ADJUNTA EL COMPROBANTE
           </p>
           <form onSubmit={handleSubmit} className={styles.form}>
             <div>
-              {/* <input
-                type="file"
-                id="comprobante"
-                onChange={handleFileChange}
-                className={styles.input}
-              /> */}
-
-              {/* <div
-                className={`${styles.uploadBox} ${
-                  dragActive ? styles.active : ""
-                }`}
-                onDragOver={handleDragOver}
-                onDragLeave={handleDragLeave}
-                onDrop={handleDrop}
-              >
-                <input
-                  type="file"
-                  id="fileUpload"
-                  className={styles.hiddenInput}
-                  onChange={handleChange}
-                />
-                <label htmlFor="fileUpload" className={styles.label}>
-                  <FiUploadCloud className={styles.icon} />
-                  {file ? (
-                    <p className={styles.fileName}>{file.name}</p>
-                  ) : (
-                    <p className={styles.text}>
-                      Arrastra y suelta el comprobante aquí o{" "}
-                      <span>haz clic para subirlo</span>
-                    </p>
-                  )}
-                </label>
-              </div> */}
-
               <div
                 className={styles.uploadContainer}
                 onClick={handleContainerClick}
@@ -145,16 +193,20 @@ function PaymentForm() {
                   ref={fileInputRef}
                   onChange={handleChange}
                   className={styles.hiddenInput}
+                  accept="image/png, image/jpeg, image/jpg, application/pdf"
                 />
               </div>
-
-              <h3 style={{ textAlign: "center", marginTop: "15px" }}>
+              <h3 style={{ textAlign: "center" }}>
+                Una vez verifiquemos la seguridad de la compra, te enviaremos el
+                PDF por mail y celular
+              </h3>
+              <h3 style={{ textAlign: "center", marginTop: "50px" }}>
                 IMPORTANTE
               </h3>
               <h4 style={{ textAlign: "center" }}>
                 Si no adjuntás el comprobante de pago, no podremos verificar la
                 seguridad de la compra. <br /> Por lo tanto, no podrás recibir
-                la guía.
+                tu compra.
               </h4>
             </div>
             <div style={{ textAlign: "center" }}>
